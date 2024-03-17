da5d561c9348e7b85ab6427a574dbd50bfeab64a
alifkhan2020 <alifkhan.bubt@gmail.com>
2024-03-18 00:36:10 +0600

    first committ

---
+++ src/Products/Products.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Product.css'
+
+export default function Products() {
+  return (
+    <div>Products</div>
+  )
+}
---
+++ src/Sidebar/Price/Price.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Price.css'
+
+
+export default function Price() {
+  return (
+    <div>Price</div>
+  )
+}
